6f91299cfa356bde50545690124b59d02d0f32cb
parmeets834 <parmeets834@gmail.com>
2021-01-27 16:36:24 +0530

    brodcast chat mechanism added

---
+++ BroadCast chat/node backend/index.js
@@ -0,0 +1,22 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+
+
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
+
+//Socket Logic
+const socketio = require('socket.io')(http)
+var nsp =socketio.of("/chatroom")
+nsp.on("connection", (userSocket) => {
+
+    userSocket.on("send_message", (data) => {
+        userSocket.emit("receive_message", data)
+        
+    })
+})
+
+http.listen(process.env.PORT)
+
+
